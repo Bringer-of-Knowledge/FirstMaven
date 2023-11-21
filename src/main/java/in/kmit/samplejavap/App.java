b42fcc30048bbe35a15d422408c0c33edc780ef2
Gowtham <nsvsrk66@gmail.com>
2023-11-21 14:06:30 +0530

    This is my First Maven Project.

---
+++ src/main/java/in/kmit/samplejavap/App.java
@@ -0,0 +1,19 @@
+package in.kmit.samplejavap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello." );
+        for(int i=0;i<150;i++)
+        {
+        	System.out.print("*");
+        }
+        System.out.println("\nSo this is my first Maven Project.");
+
+    }
+}
